d215973973ca13773f5a132fa7e35b391da8a277
Ben Mepham <25695617+benmepham@users.noreply.github.com>
2024-12-11 22:14:32 +0000

    day05 part1

--- bjm_aoc/aoc2024/day05.py
+++ bjm_aoc/aoc2024/day05.py
@@ -1,29 +1,28 @@
-from aocd import data
+from aocd import data, submit
 
 
 def part_a(data):
     data = data.split('\n\n')
-    rules = data[0].splitlines()
-    pagesets = data[1].splitlines()
-    rulesdict = {}
-    for rule in rules:
+    page_groups = [pages.split(',') for pages in data[1].splitlines()]
+    rules = {}
+    for rule in data[0].splitlines():
         split = rule.split('|')
-        if not split[0] in rulesdict:
-            rulesdict[split[0]] = []
-        rulesdict[split[0]].append(split[1])
-    print(rulesdict)
-    pagesetsf = [pages.split(',') for pages in pagesets]
-    print(pagesetsf)
-    result = []
-    for pages in pagesetsf:
-        valid = 1
-        for page in pages:
-            if page in rulesdict:
-                valid = 0
-        if valid == 1:
-            result.append(pages[(len(pages) -1 )/2])
-
-    print(result)
+        if not split[0] in rules:
+            rules[split[0]] = []
+        rules[split[0]].append(split[1])
+    # print(rules)
+    # print(page_groups)
+    result = 0
+    for pages in page_groups:
+        valid = True
+        for i, page in enumerate(pages):
+            if not page in rules.keys():
+                continue
+            for post in rules[page]:
+                if post in pages[:i]:
+                    valid = False
+        if valid:
+            result += int(pages[len(pages) // 2])
     return result
 
 
@@ -65,7 +64,14 @@ test_data = """\
 
 
 if __name__ == "__main__":
-    assert part_a(test_data) == "expected test result a"
-    assert part_b(test_data) == "expected test result b"
-    print(part_a(data))
-    print(part_b(data))
+    assert part_a(test_data) == 143
+    part_a_ans = part_a(data)
+    print(part_a_ans)
+    input("Submit?")
+    submit(part_a_ans, part='a')  
+  
+    # assert part_b(test_data) == 2
+    # part_b_ans = part_b(data)
+    # print(part_b_ans)
+    # input("Submit?")
+    # submit(part_b_ans, part='b')
