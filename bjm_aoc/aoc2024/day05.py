2f87aab973e3198ab5fd75cf48f8eb5bd764735c
Ben Mepham <25695617+benmepham@users.noreply.github.com>
2024-12-11 22:58:18 +0000

    day05 part2

--- bjm_aoc/aoc2024/day05.py
+++ bjm_aoc/aoc2024/day05.py
@@ -1,7 +1,7 @@
 from aocd import data, submit
 
 
-def part_a(data):
+def parse_data(data):
     data = data.split('\n\n')
     page_groups = [pages.split(',') for pages in data[1].splitlines()]
     rules = {}
@@ -10,8 +10,12 @@ def part_a(data):
         if not split[0] in rules:
             rules[split[0]] = []
         rules[split[0]].append(split[1])
-    # print(rules)
-    # print(page_groups)
+    return page_groups, rules
+
+
+def part_a(data):
+    page_groups, rules = parse_data(data)
+
     result = 0
     for pages in page_groups:
         valid = True
@@ -26,8 +30,38 @@ def part_a(data):
     return result
 
 
+def create_new(pages, rules):
+    new = []
+    new.append(pages[0])
+    for item in pages[1:]:
+        min_index = 1000000
+        if item in rules.keys():
+            for post in rules[item]:
+                if post in new:
+                    if new.index(post) < min_index:
+                        min_index = new.index(post)
+        if min_index != 1000000:
+            new.insert(min_index, item)
+        else:
+            new.append(item)
+    return new
+
+
 def part_b(data):
-    # more code here..
+    page_groups, rules = parse_data(data)
+
+    result = 0
+    for pages in page_groups:
+        valid = True
+        for i, page in enumerate(pages):
+            if not page in rules.keys():
+                continue
+            for post in rules[page]:
+                if post in pages[:i]:
+                    valid = False
+        if not valid:
+            pages_new = create_new(pages, rules)
+            result += int(pages_new[len(pages_new) // 2])
     return result
 
 
@@ -64,14 +98,14 @@ test_data = """\
 
 
 if __name__ == "__main__":
-    assert part_a(test_data) == 143
-    part_a_ans = part_a(data)
-    print(part_a_ans)
-    input("Submit?")
-    submit(part_a_ans, part='a')  
-  
-    # assert part_b(test_data) == 2
-    # part_b_ans = part_b(data)
-    # print(part_b_ans)
+    # assert part_a(test_data) == 143
+    # part_a_ans = part_a(data)
+    # print(part_a_ans)
     # input("Submit?")
-    # submit(part_b_ans, part='b')
+    # submit(part_a_ans, part='a')  
+  
+    assert part_b(test_data) == 123
+    part_b_ans = part_b(data)
+    print(part_b_ans)
+    input("Submit?")
+    submit(part_b_ans, part='b')
