983ff7d6c76527c4bd0af2faff01b6dd859bc108
Ben Mepham <25695617+benmepham@users.noreply.github.com>
2024-12-24 13:21:47 +0000

    day08 part2

--- bjm_aoc/aoc2024/day08.py
+++ bjm_aoc/aoc2024/day08.py
@@ -20,14 +20,29 @@ def part_a(data):
             diff = [combo[0][0] - combo[1][0], combo[0][1] - combo[1][1]]
             loc = [combo[0][0] + diff[0], combo[0][1] + diff[1]]
             if (loc[0] >= 0 and loc[0] < len(grid) and loc[1] >=0 and loc[1] < len(grid[0])):
-                locations.add(''.join(str(x) for x in loc))
+                locations.add(','.join(str(x) for x in loc))
     return len(locations)
 
 
 
 def part_b(data):
-    # more code here..
-    return result
+    grid = [list(row) for row in data.splitlines()]
+    antennas = defaultdict(list)
+    for pos_row, row in enumerate(grid):
+        for pos_col, char in enumerate(row):
+            if char.isalnum():
+                antennas[char].append([pos_row, pos_col])
+
+    locations = set()
+    for antenna in antennas:
+        combos = list(permutations(antennas[antenna], 2))
+        for combo in combos:
+            diff = [combo[0][0] - combo[1][0], combo[0][1] - combo[1][1]]
+            loc = [combo[0][0], combo[0][1]]
+            while (loc[0] >= 0 and loc[0] < len(grid) and loc[1] >=0 and loc[1] < len(grid[0])):
+                locations.add(','.join(str(x) for x in loc))
+                loc = [loc[0] + diff[0], loc[1] + diff[1]]
+    return len(locations)
 
 
 test_data = """\
@@ -64,7 +79,7 @@ if __name__ == "__main__":
     elif args.part == '2':  
         ans = part_b(test_data)
         print('test_data ans:', ans)
-        assert ans == 1
+        assert ans == 34
         ans = part_b(data)
         print('ans:', ans)
         resp = input("Submit? (y/ENTER)")
